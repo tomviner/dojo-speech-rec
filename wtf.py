761a4e63683f5700d3c03693cf79ee6c557c18e5
Madda <nobody.youknow@local>
2019-04-04 20:19:31 +0100

    Add awesome responses

--- wtf.py
+++ wtf.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python
 import speech_recognition as sr
+import subprocess
+
+
+def act_on_will_of_people(input_text):
+    respond('I understand perfectly, but this is not interesting to me.')
+
+
+def respond(message):
+    subprocess.check_call(['espeak', message])
 
 
 def rules():
@@ -9,12 +18,11 @@ def rules():
         audio = r.listen(source, phrase_time_limit=5)
 
     try:
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        act_on_will_of_people(r.recognize_sphinx(audio))
     except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
+        respond("Sphinx could not understand audio")
     except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-    # TODO: Make this do the speech recognition loop and pass off stuff to something 
+        respond("Sphinx error; {0}".format(e))
 
 
 rules()
