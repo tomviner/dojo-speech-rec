1c48d2d6cf96e2245eb7102d9209d56218f4e818
Madda <nobody.youknow@local>
2019-04-04 20:09:58 +0100

    Add WTF rules for Alexit

---
+++ wtf.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+import speech_recognition as sr
+
+
+def rules():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak clearly")
+        audio = r.listen(source)
+
+    try:
+        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    except sr.UnknownValueError:
+        print("Sphinx could not understand audio")
+    except sr.RequestError as e:
+        print("Sphinx error; {0}".format(e))
+    # TODO: Make this do the speech recognition loop and pass off stuff to something 
+
+
+rules()
